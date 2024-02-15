4eb600a1e539f72176050b6cde899ea9dc0c0ca0
KB2311 <kunjbiharihirani@gmail.com>
2024-02-15 16:31:48 +0530

    kb

--- src/components/NewsSection/index.jsx
+++ src/components/NewsSection/index.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const NewsSection = ({ mydata }) => {
   return (
-    <div className="grid w-full grid-cols-1 gap-5 px-3 pt-14 sm:grid-cols-2 lg:grid-cols-3">
+    <div className="relative grid w-full grid-cols-1 justify-center gap-5 px-3 pt-14 sm:grid-cols-2 lg:grid-cols-3">
       {mydata.map((x) => (
-        <div className="w-full overflow-hidden rounded-xl border-2 border-black">
-          <div className="img_news">
+        <div className="flex h-full w-full flex-col overflow-hidden rounded-xl border-2 border-black bg-yellow-100">
+          <div className="img_news h-60">
             <img className="" src={x.imageUrl} alt="" />
           </div>
-          <div className="bg-yellow-100 p-3">
-            <h3 className="pb-3 font-bold text-red-700">{x.title}</h3>
+          <div className="h-40 p-3">
+            <h3 className="pb-3 text-xl font-bold text-red-700">{x.title}</h3>
             <div className="">
               <p className="font-bold">
                 <span className="">By :</span> {x.author}
@@ -18,13 +18,13 @@ const NewsSection = ({ mydata }) => {
                 {x.time} on {x.date}
               </p>
             </div>
-            <div className="flex w-full justify-end pt-2 font-bold">
-              <button className="rounded-md bg-blue-300 px-2" type="button">
-                <a href={x.readMoreUrl} alt="linkofurl">
-                  Read More...
-                </a>
-              </button>
-            </div>
+          </div>
+          <div className="flex w-full justify-end px-2 pb-2 font-bold">
+            <button className="rounded-md bg-blue-300 px-2" type="button">
+              <a href={x.readMoreUrl} alt="linkofurl">
+                Read More...
+              </a>
+            </button>
           </div>
         </div>
       ))}
